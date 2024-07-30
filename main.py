3f771b246b44020c48c398b15308ad5b70188c0a
jason-sj-lee <jason.981214@gmail.com>
2024-07-30 10:06:38 -0400

    Add Count Characters

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world")
+def count_words(str):
+  words = str.split()
+  return len(words)
+
+def count_chars(str):
+  char_count = {}
+  lowered_string = str.lower()
+  for c in lowered_string:
+      if c in char_count:
+        char_count[c] += 1
+      else:
+         char_count[c] = 1
+  return char_count
+
+def main():
+  f = open("./books/frankenstein.txt")
+  book_text = f.read()
+
+  print(count_chars(book_text))
+
+if __name__ == "__main__":
+    main()
