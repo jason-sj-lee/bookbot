c7e864cf67a67e6dc63fbfc4ef151e47e6705051
jason-sj-lee <jason.981214@gmail.com>
2024-07-30 10:56:30 -0400

    Generate report

--- main.py
+++ main.py
@@ -12,11 +12,39 @@ def count_chars(str):
          char_count[c] = 1
   return char_count
 
+def sort_on(dict):
+    return dict["num"]
+
+def gen_report(book_text):
+  words = count_words(book_text)
+  chars = count_chars(book_text)
+
+  char_list = []
+
+  for c in chars:
+     if c.isalpha():
+        char_list.append({"char": c, "num": chars[c]})
+
+  char_list.sort(reverse=True, key=sort_on)
+
+  char_list_str = ''
+
+  for char in char_list:
+     char_list_str += f"The '{char['char']}' character was found {char['num']} times\n  "
+
+  return f"""
+  --- Begin report of books/frankenstein.txt ---
+  {words} words found in the document
+
+  {char_list_str}
+  --- End Report ---
+  """
+
 def main():
   f = open("./books/frankenstein.txt")
   book_text = f.read()
 
-  print(count_chars(book_text))
+  print(gen_report(book_text))
 
 if __name__ == "__main__":
     main()
